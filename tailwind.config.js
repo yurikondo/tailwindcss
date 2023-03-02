e4bd73c28f305a62331337e225237a036eefa6ed
yuri <yuri@example.com>
2023-03-03 08:32:56 +0900

    Setup tailwindcss 🔨

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// npx tailwindcss init で追加
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
