a183a0dda7ab01e5caec989b724d947c7a9872c3
yuri <yuri@example.com>
2023-03-04 05:39:50 +0900

    Change a background color 💄

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        body: "#050505",
+      },
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
       },
